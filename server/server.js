360f686bec856b71e968a3ce9ac68c45b10ad127
agcharis67705 <agcharis67705@th-nuernberg.de>
2019-10-18 17:07:03 +0200

    set up subdomain redirects for express

--- server/server.js
+++ server/server.js
@@ -2,10 +2,24 @@
 /* eslint-disable */
 
 const express = require('express')
+const subdomain = require('express-subdomain')
+
 const app = express()
-const port = 80;
+const storeRouter = express.Router()
+
+storeRouter.get('/', (req, res) => {
+    res.redirect('https://paredros-store/')
+})
+
+app.use(subdomain('store', storeRouter))
+
+
+// more subdomain routs will go here
+
 
 app.use(express.static(__dirname))
+
+const port = 80;
 
 app.listen(port, () => {
     console.log(Date() + ': paredros gateway server running on port ' + port)
