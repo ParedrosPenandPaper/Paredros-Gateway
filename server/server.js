0d52ca71c208d0dc250155f144f6bd6f51357b14
agcharis67705 <agcharis67705@th-nuernberg.de>
2019-10-18 16:28:22 +0200

    fixed static content path in express server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 const port = 80;
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname))
 
 app.listen(port, () => {
     console.log('paredros gateway server running on port ' + port)
