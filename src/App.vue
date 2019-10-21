9f1257c4d7cae34e715ef47c07a152bf1b19398f
Ismael Agchar <ismael.agchar@siemens-healthineers.com>
2019-10-21 17:47:23 +0200

    added graphical overlay for gateway server

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,105 @@
 <template>
   <div id="app">
-    welcome to Project Paredros!
+
+    <div class="routing-panel" id="auth-container">
+      <p id="auth-paragraph">login</p>
+    </div>
+
+    <div id="routing-panel-container">
+      <div id="creator-panel " class="routing-panel sized-routing-panel" @click="redirectTo('https://paredros-creator/')">
+        <p class="routing-panel-paragraph">creator</p>
+      </div>
+      <div id="store-panel" class="routing-panel sized-routing-panel" @click="redirectTo('https://paredros-store/')">
+        <p class="routing-panel-paragraph">store</p>
+      </div>
+      <div id="player-panel" class="routing-panel sized-routing-panel" @click="redirectTo('https://paredros-player/')">
+        <p class="routing-panel-paragraph">player</p>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  methods: {
+    redirectTo: function(url) {
+      window.location.href = url
+    }
+  }
 }
 </script>
 
 <style>
 #app {
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+#auth-container{
+  position: absolute;
+  top: 0;
+  right: 0;
+
+  margin: 10px;
+}
+
+#auth-paragraph{
+  margin: 1em;
+  font-size: 1.5em;
+}
+
+#routing-panel-container{
+  width: 100%;
+
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+  justify-items: center;
+}
+
+.sized-routing-panel{
+  width: 20vw;
+  height: 20vw;
+  min-width: 200px;
+  min-height: 200px;
+
+  margin: 50px;
+}
+
+.routing-panel{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: 1px solid darkgrey;
+  border-radius: 10%;
+
+  background-color: #7eaacd;
+
+  cursor: pointer;
+}
+
+.routing-panel:hover{
+  background-color: #a3c2db;
+}
+
+.routing-panel:active{
+  background-color: steelblue;
+}
+
+.routing-panel-paragraph{
+  margin: 0;
+  font-size: 2em;
+  color: black;
 }
 </style>
